d9e83430f62258803ae76f131b230c082768a825
Mehdi Behira <mehdibehira@gmail.com>
2021-10-21 17:48:41 +0200

    Create ZooTest2.java

---
+++ src/main/java/ZooTest2.java
@@ -0,0 +1,32 @@
+
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ZooTest2 {
+
+	@Ignore
+	@Test
+	public void unTest()  {
+		zoo zoo1 = new zoo();
+		String expectedMessage = "Nombre de visiteur max atteint";
+
+		for (int i =0 ; i < 20; i++) {
+			try {
+				zoo1.nouveauVisiteur();
+			} catch (LimiteVisiteurException e) {
+				String actualMessage = e.getMessage();
+			    assertTrue(actualMessage.contains(expectedMessage));
+
+				e.printStackTrace();
+			}
+		}
+
+	    
+
+
+    }
+}
